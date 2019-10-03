0a96082598c7203c7bbe357876284e97333c2e68
apisit <nikola>
2019-10-03 14:46:52 +0200

    -

---
+++ SaaSApiDemo/UseCases/Users/AddUser/AddUserModel.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace SaaSApiDemo.UseCases.Users.AddUser
+{
+    public class AddUserModel
+    {
+        public class AddUserData
+        {
+            public string FirstName { get; set; }
+            public string LastName { get; set; }
+            public string Username { get; set; }
+            public string Email { get; set; }
+            public string UserId { get; set; }
+            public string ContactInfo { get; set; }
+        }
+
+        [FromBody] public AddUserData Data { get; set; }
+        [FromRoute(Name = "tenantId")] public Guid TenantID { get; set; }
+    }
+}
---
+++ SaaSApiDemo/UseCases/Users/DeleteUser/UsersController.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace SaaSApiDemo.UseCases.Users.DeleteUser
+{
+    [ApiController]
+    [ApiVersion("1.0")]
+    [Produces("application/json")]
+    [Route("api/v{version:apiVersion}/Tenants/{tenantId}/[controller]")]
+    public class UsersController : ControllerBase
+    {
+        [HttpDelete()]
+        [Route("{tenantId:guid}")]
+        public async Task<IActionResult> DeleteAsync([FromRoute]Guid tenantId) => Ok(tenantId);
+    }
+}
