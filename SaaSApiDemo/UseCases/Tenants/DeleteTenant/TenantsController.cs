9115e654292af5c0081e4489549a81e5ed716606
apisit <nikola>
2019-10-03 13:38:22 +0200

    -

---
+++ SaaSApiDemo/UseCases/Tenants/DeleteTenant/TenantsController.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace SaaSApiDemo.UseCases.Tenants.DeleteTenant
+{
+    [ApiController]
+    [ApiVersion("1.0")]
+    [Produces("application/json")]
+    [Route("api/v{version:apiVersion}/[controller]")]
+    public class TenantsController : ControllerBase
+    {
+        [HttpDelete()]
+        [Route("{tenantId:guid}")]
+        public async Task<IActionResult> DeleteAsync([FromRoute]Guid tenantId) => Ok(tenantId);
+    }
+}
