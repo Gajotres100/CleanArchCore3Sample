8aa6ed2747c1750a9d07d041f1c62184a2417f4b
apisit <nikola>
2019-10-03 14:52:07 +0200

    -

--- SaaSApiDemo/UseCases/Users/UpdateUser/UpdateUserModel.cs
+++ SaaSApiDemo/UseCases/Users/UpdateUser/UpdateUserModel.cs
@@ -4,22 +4,21 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 
-namespace SaaSApiDemo.UseCases.Users.AddUser
+namespace SaaSApiDemo.UseCases.Users.UpdateUser
 {
-    public class AddUserModel
+    public class UpdateUserModel
     {
-        public class AddUserData
+        public class UpdateUserData
         {
             [FromBody] public string FirstName { get; set; }
             [FromBody] public string LastName { get; set; }
-            [FromBody] public string Username { get; set; }
             [FromBody] public string Email { get; set; }
-            [FromBody] public string UserId { get; set; }
             [FromBody] public string ContactInfo { get; set; }
             [FromBody] public string TransactionId { get; set; }
         }
 
-        [FromBody] public AddUserData Data { get; set; }
-        [FromRoute(Name = "tenantId")] public Guid TenantID { get; set; }
+        [FromBody] public UpdateUserData Data { get; set; }
+        [FromRoute(Name = "tenantId")] public Guid TenantId { get; set; }
+        [FromRoute(Name = "userId")] public Guid UserId { get; set; }
     }
 }
---
+++ SaaSApiDemo/UseCases/Users/UpdateUser/UsersController.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace SaaSApiDemo.UseCases.Users.UpdateUser
+{
+    [ApiController]
+    [ApiVersion("1.0")]
+    [Produces("application/json")]
+    [Route("api/v{version:apiVersion}/Tenants/{tenantId}/[controller]")]
+    public class UsersController : ControllerBase
+    {
+        [HttpPut()]
+        [Route("{userId:guid}")]
+        public async Task<IActionResult> GetAsync([FromRoute]UpdateUserModel data) => Ok(data);
+    }
+}
