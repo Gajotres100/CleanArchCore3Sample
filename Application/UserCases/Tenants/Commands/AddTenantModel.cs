1cde00d56fbff56ca69b85c9af3b534d33aeb64e
apisit <nikola>
2019-10-04 10:33:20 +0200

    -

--- Application/UserCases/Tenants/Commands/AddTenantModel.cs
+++ Application/UserCases/Tenants/Commands/AddTenantModel.cs
@@ -4,7 +4,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 
-namespace SaaSApiDemo.UseCases.Tenants.AddTenant
+namespace Application.UserCases.Tenants.Commands
 {
     public class AddTenantModel
     {
